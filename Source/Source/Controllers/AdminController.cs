6b50d6c340ed7dc73271aaedcec7b3eed9628918
Thanh <ducthanh98a@gmail.com>
2019-03-07 10:54:55 +0700

    sửa database, admincontroller

---
+++ Source/Source/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Source.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult ChiNhanh()
+        {
+            return View();
+        }
+
+        public ActionResult ChiPhi()
+        {
+            return View();
+        }
+
+        public ActionResult Combo()
+        {
+            return View();
+        }
+
+        public ActionResult SanPham()
+        {
+            return View();
+        }
+
+        public ActionResult KhuyenMai()
+        {
+            return View();
+        }
+
+        public ActionResult LoaiChiPhi()
+        {
+            return View();
+        }
+
+        public ActionResult LoaiSanPham()
+        {
+            return View();
+        }
+
+        public ActionResult TaiKhoan()
+        {
+            return View();
+        }
+    }
+}
