172179663cb0ddcdbd7eae09a6ebb4ce1c791f39
Thanh <ducthanh98a@gmail.com>
2019-05-05 17:39:57 +0700

    update product

---
+++ Source/DAO/Combo_REL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Combo_REL
+    {
+        public int MaCombo { get; set; }
+        public string TenCombo { get; set; }
+    }
+}
