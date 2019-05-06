06965f74a47505d12599bec3867c65ee8e95093a
Hùng Nguyễn <tuanhung.1998@gmail.com>
2019-05-06 16:20:26 +0700

    update chiphi

---
+++ Source/DAO/ChiPhiDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class ChiPhiDTO
+    {
+        public int MaChiPhi { set; get; }
+        public decimal GiaTien { set; get; }
+        public DateTime NgayTao { set; get; }
+        public int MaLoaiChiPhi { set; get; }
+    }
+}
