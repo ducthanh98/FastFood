7c740c277cd956dc3509caed902a9847586f2dd3
Hùng Nguyễn <tuanhung.1998@gmail.com>
2019-05-11 16:01:02 +0700

    LayOutSP

---
+++ Source/Source/Controllers/LoaiSanPhamController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DTO;
+using Source.Areas.Admin.Controllers;
+
+namespace Source.Controllers
+{
+    public class LoaiSanPhamController : BaseController
+    {
+        // GET: MenuProduct
+        public ActionResult LoaiSanPham()
+        {
+            List<LoaiSanPhamDTO> list = LoaiSanPham_Service.GetAll();
+            return View(list);
+        }
+
+        public ActionResult ListMenu()
+        {
+            List<LoaiSanPhamDTO> list = new List<LoaiSanPhamDTO>();
+            return View(list);
+        }
+    }
+}
